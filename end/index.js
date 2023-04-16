2e5d6287a8b37d858c2e4376323190a38b0acf07
DiwanshGupta <dewanshgupta120@gmail.com>
2023-04-16 11:48:22 +0530

    Gossipfox

---
+++ end/index.js
@@ -0,0 +1,29 @@
+// server for Gosssipfox
+const io = require("socket.io")(800, {
+  cors: {
+    origin: "http://localhost:5501",
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (Name) => {
+    console.log("Name user", Name);
+    users[socket.id] = Name;
+    socket.broadcast.emit("user-joined", Name);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+
+    socket.on("disconnect", (message) => {
+      socket.broadcast.emit("left", users[socket.id]);
+      delete users[socket.id];
+    });
+  });
+});
